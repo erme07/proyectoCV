c7170f8e1dbb15722d3887ef89647538da59a0eb
erme07 <erik.medina07@gmail.com>
2022-07-29 09:33:13 -0300

    probando nueva forma de subir foto

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 const getLink = () =>{
     let inputLink = document.getElementById("linkFoto").value; 
     document.getElementById("foto").src=inputLink;
-  }
-  var cont=0;
-  const mostrarPanel = () =>{
-    
+}
+
+var cont=0;
+const mostrarPanel = () =>{
     cont++;
     if(cont==1){
         document.getElementById("Panel").style.display = ''; // hide
@@ -14,3 +14,22 @@ const getLink = () =>{
         document.getElementById("Panel").style.display = 'none'; // hide
     }
 }
+
+function mostrarImagen(event){
+    var imagenSource = event.target.result;
+    var previewImage = document.getElementById('preview');
+
+    previewImage.src = imagenSource;
+}
+
+function procesarArchivo(event){
+    var imagen = event.target.files[0];
+
+    var lector = new FileReader();
+
+    lector.addEventListener('load', mostrarImagen, false);
+
+    lector.readAsDataURL(imagen);
+}
+
+document.getElementById('archivo').addEventListener('change', procesarArchivo, false)
